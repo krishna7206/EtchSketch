fc4cceab3bd7bd8c670f23b0375a8471dc6b9df3
Krishna Nambiar <knambiar41@gmail.com>
2022-10-04 17:18:08 +0530

    initial version of etchsketc 16x16

--- externaljs.js
+++ externaljs.js
@@ -1,5 +1,25 @@
 
+function gridSize()
+{
+let x = document.getElementById("gridvalue").value;
+console.log(x)
 let container = document.getElementById('container');
+container.style.setProperty('--grid-rows', x);
+container.style.setProperty('--grid-cols', x);
+for(let i=0;i<(x*x);i++)
+{
 let box = document.createElement('div')
-box.className = 'box'
-container.appendChild(box);
+container.appendChild(box).className = "box";
+}
+
+document.querySelectorAll(".box").forEach(item => {
+item.addEventListener('mouseover', event => {
+item.style.backgroundColor = "red";
+})
+})
+}
+
+function reset()
+{
+   location.reload()
+}
